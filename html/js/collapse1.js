f8b2c4d65626aef6c74dbfb4a242fce88bf8c781
Igho-Godwin <ighogodwinavweroswo@gmail.com>
2017-10-18 13:49:03 +0100

    suspending data collection

---
+++ html/js/collapse1.js
@@ -0,0 +1,22 @@
+    
+function collapse1()
+{
+  $('#collapseOne').collapse("show");
+        
+}
+
+    
+function collapse2()
+{
+  $('#collapseTwo').collapse("show");
+        
+}
+
+function collapse3()
+{
+  $('#collapseThree').collapse("show");
+        
+}
+    
+
+    
